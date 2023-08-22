f71c8084536999937de8a71953fd1ebc6e99b83c
abraham <abrahamsuarezolay@gmail.com>
2023-08-22 23:01:56 +0100

    Added find by username to the UserVO repository, returning a UserDetails type.

--- main/java/com/parkbuddy/ParkBuddy/repositories/UserRepository.java
+++ main/java/com/parkbuddy/ParkBuddy/repositories/UserRepository.java
@@ -2,8 +2,11 @@ package com.parkbuddy.ParkBuddy.repositories;
 
 import com.parkbuddy.ParkBuddy.model.UserVO;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends CrudRepository<UserVO, Integer> {
+
+    UserDetails findByUsername(String username);
 }
